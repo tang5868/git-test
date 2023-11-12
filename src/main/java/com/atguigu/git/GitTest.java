4507cb882bce863f55b2f1175460a83e43c9ecb1
tang5868 <13965835868@163.com>
2023-11-12 19:32:38 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git 2");
         System.out.println("hello git 3");
+        System.out.println("hello git 4 hot-fix");
     }
 }
