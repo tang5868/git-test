32aac8ac7adb4a633b18c512ec787327fb6c8754
tang5868 <13965835868@163.com>
2023-11-12 16:10:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.atguigu.git
+ * Description:
+ *
+ * @Author:
+ * @Create: 2023/11/12 15:58
+ * @Version: v1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
